6a3110708dcc34cbaf33e2fd8ee04246406709ee
Mohitkaila <mohit.kaila@ucalgary.ca>
2024-01-23 15:13:42 -0700

    minor changes

--- Part 3/p3.py
+++ Part 3/p3.py
@@ -15,12 +15,9 @@ def separate_by_income(data):
 
     return below_10000, at_or_above_10000
 
-# Function to plot histogram
-def plot_histogram(data, filename, title):
-    # Extracting internet usage rates
-    internet_usage = [country["internetuserate"] for country in data if country["internetuserate"] is not None]
 
-    # Plotting
+def plot_histogram(data, filename, title):
+    internet_usage = [country["internetuserate"] for country in data if country["internetuserate"] is not None]
     plt.figure(figsize=(10, 6))
     plt.hist(internet_usage, bins=20, color='blue', edgecolor='black')
     plt.title(title)
@@ -29,13 +26,10 @@ def plot_histogram(data, filename, title):
     plt.savefig(filename)
     plt.close()
 
-# Reading data from the JSON file
 with open('Part 3/internetdata.json', 'r') as file:
     data = json.load(file)
 
-# Separating the dataset
 below_10000, at_or_above_10000 = separate_by_income(data)
 
-# Plot histograms
 plot_histogram(below_10000, 'hist1.png', 'Internet Usage in Countries with Income below $10,000')
 plot_histogram(at_or_above_10000, 'hist2.png', 'Internet Usage in Countries with Income at or above $10,000')
